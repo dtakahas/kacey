d47c42fd425903a55cd9f9d75521227efdd1c6e4
Dave Takahashi <davidt@unity3d.com>
2018-06-05 17:41:10 -0700

    Selection

--- src/kacey.cr
+++ src/kacey.cr
@@ -7,15 +7,26 @@ puts "Clusters:"
 f = File.read("/Users/davetakahashi/.kube/config")
 data = YAML.parse(f)
 
-data["clusters"].each do |cluster|
-  puts cluster["name"]
+data["clusters"].each_with_index do |cluster, i|
+  puts "#{i + 1}. #{cluster["name"]}"
 end
 
 puts "\n"
 
 puts "Contexts:"
 
-data["contexts"].each do |context|
-  puts context["name"]
-  puts context["context"]["cluster"]
+data["contexts"].each_with_index do |context, i|
+  puts "#{i + 1}. #{context["name"]} (cluster: #{context["context"]["cluster"]})"
+end
+
+puts "\n"
+
+puts "Select context: "
+
+selection = gets || "none"
+
+if selection == "none"
+  puts "Exiting"
+else
+  puts data["contexts"][selection.to_i - 1]
 end
